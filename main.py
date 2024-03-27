afeb87522d88ebc3dbe32cdc7d886caf3b0104b7
Kanashii47 <UnbidHealer.com>
2024-03-27 23:17:01 +0300

    nachalo

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello")
-print("world")
+class student():
+    """descip of students(name,surname,age,year)"""
+    def __init__(self,name,surname,age,year):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.year = year
+    
+    def review(self):
+        """printing data of student"""
+        print("Student " + self.surname + self.name + "and" + self.age + "years old currently on" + self.year + "year of edu")
+student1 = student(input(str()),)
